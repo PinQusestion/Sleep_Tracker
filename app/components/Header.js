fcdb655ee0c680f0f6f77c3f513a84fdf125bfad
PinQusestion <PinQusestion@users.noreply.github.com>
2025-07-18 08:06:10 +0530

    Designed the dashboard

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,19 +4,9 @@ import React from 'react'
 import Profile from './Profile';
 
 import { useAuth } from '../../lib/authContext';
-import { logOut } from '../../lib/authService';
 
 function Header() {
   const {user, loading} = useAuth();
-
-  const clickHandler = async () => {
-    try {
-      await logOut();
-      window.location.href = "/";
-    } catch (err) {
-      alert(err.message);
-    }
-  };
 
   return (
     <div className='w-full bg-[#0a0e18] flex justify-between items-center p-3 pt-4 pb-4 border-b-1 border-white'>
